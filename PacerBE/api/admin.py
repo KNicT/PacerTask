45fc3551482cd892723655a09ec30dad63e1a79b
Nicholson <108812337+KNicT@users.noreply.github.com>
2023-06-02 01:02:54 +0800

    Update admin.py

--- PacerBE/api/admin.py
+++ PacerBE/api/admin.py
@@ -4,9 +4,9 @@ from api.models import Scores
 
 # Register your models here.
 class AdminScores(admin.ModelAdmin):
-    list_display = ['id', 'userId', 'score']
+    list_display = ['id', 'userId', 'score'] #'is_adult'
     list_editable = ['userId', 'score']
     search_fields = ['userId']
-    list_filter = ['score']
+    list_filter = ['score'] #'is_adult'
 
 admin.site.register(Scores, AdminScores)
