c9d2dd509306bfe31aacb5316414a51ae6db3af4
Nicholson <108812337+KNicT@users.noreply.github.com>
2023-06-02 01:03:13 +0800

    Update views.py

--- PacerBE/api/views.py
+++ PacerBE/api/views.py
@@ -1,11 +1,12 @@
 #API Endpoint
 from django.http import JsonResponse
 from api.models import Scores
+import random
 def get_score(request):
-    val = 100
-    result = val + 1
+    val = 0 
+    result = random.randint(val, 100)
 
-    userId = request.GET.get('userId', '')
+    userId = request.GET.get('userId', 'test2')
     Scores.objects.create(userId=userId, score=result)
 
     response = {'result': result}
