a84a41571ba24049190f3d48cda1aeee19987a49
Nicholson <108812337+KNicT@users.noreply.github.com>
2023-06-02 01:02:34 +0800

    Update models.py

--- PacerBE/api/models.py
+++ PacerBE/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Scores(models.Model):
     userId = models.CharField(max_length=100)
     score = models.IntegerField()
+    # is_adult = models.BooleanField(default=True)
 
     def __str__(self):
         return f"User ID: {self.userId}, Score: {self.score}"
