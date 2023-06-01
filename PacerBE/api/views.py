e3095500f8b4a110e5aa0b15896d4bad736de5d7
Nicholson <108812337+KNicT@users.noreply.github.com>
2023-06-01 15:36:03 +0800

    Update views.py

--- PacerBE/api/views.py
+++ PacerBE/api/views.py
@@ -2,17 +2,11 @@
 from django.http import JsonResponse
 from api.models import Scores
 def get_score(request):
-    if request.method == 'POST':
-        userId = request.POST.get('userId', '')
-        score = request.POST.get('score', '')
-        #static request to add a value to the key userId
-        # userId = request.GET.get('userId', 'JARSO')
+    val = 100
+    result = val + 1
 
-        # score = 100
-        result = int(score) + 1
-        Scores.objects.create(userId=userId, score = result)
+    userId = request.GET.get('userId', '')
+    Scores.objects.create(userId=userId, score=result)
 
-        response = {'result' : result}
-        return JsonResponse(response, safe=False)
-    else:
-        return JsonResponse({'message': 'Invalid Request Method'})
+    response = {'result': result}
+    return JsonResponse(response)
